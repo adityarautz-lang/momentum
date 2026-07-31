e124c04f00721297c58a1eaaba1c11914a740e84
adityarautz-lang <adityarautz@gmail.com>
2026-07-31 10:27:21 +0530

    clear focus stack

--- app/page.tsx
+++ app/page.tsx
@@ -9208,7 +9208,7 @@ aria-label="Focus workspace"
   className={`min-w-0 self-start rounded-[14px] border p-5 shadow-[0_1px_2px_rgba(15,23,42,0.02)] xl:p-6 ${dashboardBorder} ${dashboardSurface}`}
 >
               <FocusModePanel
-prioritizedTasks={prioritizedTasks}
+prioritizedTasks={planningTasks}
 completedToday={completedToday}
 insightsHistory={insightsHistory}
 userPlanningProfile={userPlanningProfile}
